da2d63e4201c5f4f2da823270d4be84473543304
Sunny <sunny6464n@gmail.com>
2022-01-30 18:20:35 +0530

    Change the time.sleep to one hour

--- Drink_Water_Notification/main.py
+++ Drink_Water_Notification/main.py
@@ -2,12 +2,13 @@ import time
 from plyer import notification
 
 if __name__ == "__main__": 
-   notification.notify(
-      title = "Drink Water fool You fool",
-      message = "Drinking water = glowing skin and a lot more",
-      app_name = 'For Sunny',
-      app_icon = 'F:\Mini Projects\Drink_Water_Notification\icon.ico',
-      timeout = 10,
-      toast = False
-      )
-   time.sleep(5)
+   while True: 
+      notification.notify(
+       title = "Drink Water fool You fool",
+       message = "Drinking water = glowing skin and a lot more",
+       app_name = 'For Sunny',
+       app_icon = 'F:\Mini Projects\Drink_Water_Notification\icon.ico',
+       timeout = 10,
+       toast = False
+       )
+      time.sleep(60*60)
