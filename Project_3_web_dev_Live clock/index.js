ed571f60502511c9a9b58897cdb515158e89040b
Sunny <sunny6464n@gmail.com>
2022-03-03 14:45:05 +0530

    Removing Project_3_Web_dev_Live. Already exist

--- Project_3_web_dev_Live clock/index.js
+++
@@ -1,6 +0,0 @@
-setInterval(() => {
-    let a = new Date();
-    let date = a.toLocaleDateString();
-    let time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds();
-    document.getElementById('clock').innerHTML = time + " On " + date;
-}, 1000);
